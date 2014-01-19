8c21c78a0d268af65441fc205ee0c0ca8318e425
Bruno17 <b.perner@gmx.de>
2014-01-19 18:19:16 +0100

    add mmlField - snippet to get translated single-fields from other resources

---
+++ core/components/migxmultilang/elements/snippets/mmlfield.snippet.php
@@ -0,0 +1,12 @@
+/*
+does only work after this commits/pulls: 
+https://github.com/bezumkin/pdoTools/commit/1f4ae742a27c69b5e299d7f1ffa75b2cd367c616
+https://github.com/bezumkin/pdoTools/pull/37
+*/
+
+$field = $modx->getOption('field',$scriptProperties,'');
+
+$scriptProperties['field'] = 'mml.' . $field;
+$scriptProperties['default'] = $modx->getOption('default',$scriptProperties, $field);
+
+return $modx->runSnippet('pdoField',$scriptProperties);
